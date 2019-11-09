14a7caa37c59dd92bd2f53c8cda75b2241346d87
uemura junya <uemura.junya@gmail.com>
2019-11-09 18:06:12 +0900

    試行錯誤中。

--- logger.h
+++ logger.h
@@ -4,18 +4,15 @@
 #error
 #endif
 
-#include <cstring>
-#include <cwchar>
 #include <sstream>
-#include <system_error>
 
 #define LOGGER_MACRO_NUMBER2TEXT(n)	#n
-#define LOGGER_MACRO_HEADER(file, line)	file "(" LOGGER_MACRO_NUMBER2TEXT(line) "):"
+#define LOGGER_MACRO_HEADER(file, line)	file "(" LOGGER_MACRO_NUMBER2TEXT(line) "): "
 
 #if defined(_UNICODE)
-#define FAIL(...)	if (logger::g_level1<wchar_t>) { logger::output(logger::g_level1<wchar_t>, LOGGER_MACRO_HEADER(__FILE__, __LINE__), __func__, __VA_ARGS__); }
-#define WARN(...)	if (logger::g_level2<wchar_t>) { logger::output(logger::g_level2<wchar_t>, LOGGER_MACRO_HEADER(__FILE__, __LINE__), __func__, __VA_ARGS__); }
-#define INFO(...)	if (logger::g_level3<wchar_t>) { logger::output(logger::g_level3<wchar_t>, LOGGER_MACRO_HEADER(__FILE__, __LINE__), __func__, __VA_ARGS__); }
+#define FAIL(...)	if (logger::g_level1<wchar_t>) { logger::output(logger::g_level1<wchar_t>, TEXT(LOGGER_MACRO_HEADER(__FILE__, __LINE__)), __func__, __VA_ARGS__); }
+#define WARN(...)	if (logger::g_level2<wchar_t>) { logger::output(logger::g_level2<wchar_t>, TEXT(LOGGER_MACRO_HEADER(__FILE__, __LINE__)), __func__, __VA_ARGS__); }
+#define INFO(...)	if (logger::g_level3<wchar_t>) { logger::output(logger::g_level3<wchar_t>, TEXT(LOGGER_MACRO_HEADER(__FILE__, __LINE__)), __func__, __VA_ARGS__); }
 #else
 #define FAIL(...)	if (logger::g_level1<char>) { logger::output(logger::g_level1<char>, LOGGER_MACRO_HEADER(__FILE__, __LINE__), __func__, __VA_ARGS__); }
 #define WARN(...)	if (logger::g_level2<char>) { logger::output(logger::g_level2<char>, LOGGER_MACRO_HEADER(__FILE__, __LINE__), __func__, __VA_ARGS__); }
@@ -24,8 +21,6 @@
 
 namespace logger
 {
-	void Initialize(int level);
-
 	template <typename char_type>
 	using Log = void (*)(std::basic_stringstream<char_type> & msg);
 
@@ -52,138 +47,127 @@ namespace logger
 	};
 
 	template <typename char_type, typename ... param_types>
-	void output(Log<char_type> log, const char * header, param_types && ... params)
+	void output(Log<char_type> log, const char_type * header, param_types && ... params)
 	{
 		std::basic_stringstream<char_type> buf;
 		(message<char_type>{ (buf << header) } << ... << std::move(params));
 		log(buf);
 	}
+}
+
+namespace logger
+{
+	template <int digits>
+	struct hexadecimal;
+
+	template <>
+	struct hexadecimal<8>
+	{
+		const std::uint8_t value;
+	};
+
+	template <>
+	struct hexadecimal<16>
+	{
+		const std::uint16_t value;
+	};
+
+	template <>
+	struct hexadecimal<32>
+	{
+		const std::uint32_t value;
+	};
+
+	template <>
+	struct hexadecimal<64>
+	{
+		const std::uint64_t value;
+	};
+
+	template <typename T>
+	hexadecimal<std::numeric_limits<std::make_unsigned_t<T>>::digits> hex(const T value)
+	{
+		return { (std::make_unsigned_t<T>) value };
+	}
 
 	template <typename char_type>
-	void print(std::basic_ostream<char_type> & os, std::uint8_t value)
+	std::basic_ostream<char_type> & print(std::basic_ostream<char_type> & os, std::uint8_t value)
 	{
 		os << "0123456789ABCDEF"[value / 16];
 		os << "0123456789ABCDEF"[value % 16];
+		return os;
 	}
 
 	template <>
-	void print(std::basic_ostream<wchar_t> & os, std::uint8_t value)
+	std::basic_ostream<wchar_t> & print(std::basic_ostream<wchar_t> & os, std::uint8_t value)
 	{
 		os << L"0123456789ABCDEF"[value / 16];
 		os << L"0123456789ABCDEF"[value % 16];
+		return os;
 	}
 
 	template <typename char_type>
-	void print(std::basic_ostream<char_type> & os, std::uint16_t value)
+	std::basic_ostream<char_type> & print(std::basic_ostream<char_type> & os, std::uint16_t value)
 	{
 		print(os, static_cast<std::uint8_t>(value >> 8));
 		print(os, static_cast<std::uint8_t>(value));
+		return os;
 	}
 
 	template <typename char_type>
-	void print(std::basic_ostream<char_type> & os, std::uint32_t value)
+	std::basic_ostream<char_type> & print(std::basic_ostream<char_type> & os, std::uint32_t value)
 	{
 		print(os, static_cast<std::uint16_t>(value >> 16));
 		print(os, static_cast<std::uint16_t>(value));
+		return os;
 	}
 
 	template <typename char_type>
-	void print(std::basic_ostream<char_type> & os, std::uint64_t value)
+	std::basic_ostream<char_type> & print(std::basic_ostream<char_type> & os, std::uint64_t value)
 	{
 		print(os, static_cast<std::uint32_t>(value >> 32));
 		print(os, static_cast<std::uint32_t>(value));
-	}
-
-	template <int digits>
-	struct make_uint {};
-
-	template <>
-	struct make_uint<8> {
-		using type = std::uint8_t;
-	};
-
-	template <>
-	struct make_uint<16> {
-		using type = std::uint16_t;
-	};
-
-	template <>
-	struct make_uint<32> {
-		using type = std::uint32_t;
-	};
-
-	template <>
-	struct make_uint<64> {
-		using type = std::uint64_t;
-	};
-
-	template <typename T>
-	using make_uint_t = typename make_uint<std::numeric_limits<std::make_unsigned_t<T>>::digits>::type;
-
-	template <typename T>
-	struct hex
-	{
-		make_uint_t<T> value;
-
-		hex(const T v) : value(v) {}
-	};
-
-	template <typename char_type, typename T>
-	std::basic_ostream<char_type> & operator<<(std::basic_ostream<char_type> & os, const hex<T> & hex)
-	{
-		os << "0x";
-		print(os, hex.value);
 		return os;
 	}
 
-	std::basic_ostream<wchar_t> & operator<<(std::basic_ostream<wchar_t> & os, const char * ptr)
+	template <typename char_type, int digits>
+	std::basic_ostream<char_type> & operator<<(std::basic_ostream<char_type> & os, const hexadecimal<digits> & hex)
 	{
-		std::mbstate_t state{};
-		wchar_t wc{};
-
-		std::size_t ret, len = std::strlen(ptr);
-
-		while (static_cast<int>(ret = std::mbrtowc(&wc, ptr, len, &state)) > 0)
-		{
-			ptr += ret, len -= ret;
-			os << wc;
-		}
-
-		return os;
+		return print(os << "0x", hex.value);
 	}
+}
 
-	struct what
+#include <system_error>
+#include <cstring>
+#include <cwchar>
+
+namespace logger
+{
+	struct system_error_code
 	{
-		int value;
+		const int value;
+	};
 
-		what(int v) : value(v) {}
+	system_error_code what(int code)
+	{
+		return { code };
 	};
 
 	template <typename char_type>
-	std::basic_ostream<char_type> & operator<<(std::basic_ostream<char_type> & os, const what & what)
+	std::basic_ostream<char_type> & print(std::basic_ostream<char_type> & os, const system_error_code & code)
 	{
-		if (what.value >= 0)
-			os << what.value << ' ';
-		else
-			os << hex(what.value) << ' ';
-
-		return os << std::system_error(what.value, std::system_category()).what();
+		return os << std::system_error(code.value, std::system_category()).what();
 	}
-#if 0
+
 	template <>
-	std::basic_ostream<wchar_t> & operator<<(std::basic_ostream<wchar_t> & os, const what & what)
+	std::basic_ostream<wchar_t> & print(std::basic_ostream<wchar_t> & os, const system_error_code & code)
 	{
-		if (what.value >= 0)
-			os << what.value << L' ';
-		else
-			os << hex(what.value) << L' ';
-
-		std::system_error syserr(what.value, std::system_category());
-		std::mbstate_t state{};
-		wchar_t wc{};
-
+		std::system_error syserr(code.value, std::system_category());
 		const char * ptr = syserr.what();
-		std::size_t ret, len = std::strlen(ptr);
+
+		std::mbstate_t state{};
+		std::size_t len = std::strlen(ptr), ret;
+		wchar_t wc{};
 
 		while (static_cast<int>(ret = std::mbrtowc(&wc, ptr, len, &state)) > 0)
 		{
@@ -193,64 +177,13 @@ namespace logger
 
 		return os;
 	}
-#endif
-}
 
-#if defined(_WIN32)
-
-#define _WIN32_WINNT _WIN32_WINNT_WIN7
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-
-namespace logger
-{
-	inline void Initialize(int level)
+	template <typename char_type>
+	std::basic_ostream<char_type> & operator<<(std::basic_ostream<char_type> & os, const system_error_code & code)
 	{
-		if (::IsDebuggerPresent())
-		{
-#if defined(_UNICODE)
-			switch (level)
-			{
-			case 3:
-				g_level3<wchar_t> = [](std::basic_stringstream<wchar_t> & msg) { msg << std::endl; ::OutputDebugStringW(msg.str().c_str()); };
-			case 2:
-				g_level2<wchar_t> = [](std::basic_stringstream<wchar_t> & msg) { msg << std::endl; ::OutputDebugStringW(msg.str().c_str()); };
-			case 1:
-				g_level1<wchar_t> = [](std::basic_stringstream<wchar_t> & msg) { msg << std::endl; ::OutputDebugStringW(msg.str().c_str()); };
-			}
-#else
-			switch (level)
-			{
-			case 3:
-				g_level3<char> = [](std::basic_stringstream<char> & msg) { msg << std::endl; ::OutputDebugStringA(msg.str().c_str()); };
-			case 2:
-				g_level2<char> = [](std::basic_stringstream<char> & msg) { msg << std::endl; ::OutputDebugStringA(msg.str().c_str()); };
-			case 1:
-				g_level1<char> = [](std::basic_stringstream<char> & msg) { msg << std::endl; ::OutputDebugStringA(msg.str().c_str()); };
-			}
-#endif
-		}
+		if (code.value >= 0)
+			return print(os << code.value << ' ', code);
+		else
+			return print(os << hex(code.value) << ' ', code);
 	}
 }
-
-#else
-
-#include <iostream>
-
-namespace logger
-{
-	inline void Initialize(int level)
-	{
-		switch (level)
-		{
-		case 3:
-			g_level3<char> = [](std::basic_stringstream<char> & msg) { std::cout << msg.str() << std::endl; };
-		case 2:
-			g_level2<char> = [](std::basic_stringstream<char> & msg) { std::cout << msg.str() << std::endl; };
-		case 1:
-			g_level1<char> = [](std::basic_stringstream<char> & msg) { std::cout << msg.str() << std::endl; };
-		}
-	}
-}
-
-#endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <clocale>
 #include "logger.h"
 
+#if defined(_WIN32)
+
+#define _WIN32_WINNT _WIN32_WINNT_WIN7
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+void Initialize()
+{
+	if (::IsDebuggerPresent())
+	{
+#if defined(_UNICODE)
+		logger::g_level3<wchar_t> = [](std::basic_stringstream<wchar_t> & msg) { msg << std::endl; ::OutputDebugStringW(msg.str().c_str()); };
+		logger::g_level2<wchar_t> = [](std::basic_stringstream<wchar_t> & msg) { msg << std::endl; ::OutputDebugStringW(msg.str().c_str()); };
+		logger::g_level1<wchar_t> = [](std::basic_stringstream<wchar_t> & msg) { msg << std::endl; ::OutputDebugStringW(msg.str().c_str()); };
+#else
+		logger::g_level3<char> = [](std::basic_stringstream<char> & msg) { msg << std::endl; ::OutputDebugStringA(msg.str().c_str()); };
+		logger::g_level2<char> = [](std::basic_stringstream<char> & msg) { msg << std::endl; ::OutputDebugStringA(msg.str().c_str()); };
+		logger::g_level1<char> = [](std::basic_stringstream<char> & msg) { msg << std::endl; ::OutputDebugStringA(msg.str().c_str()); };
+#endif
+	}
+}
+
+#else
+
+#include <iostream>
+
+void Initialize()
+{
+	logger::g_level3<char> = [](std::basic_stringstream<char> & msg) { std::cout << msg.str() << std::endl; };
+	logger::g_level2<char> = [](std::basic_stringstream<char> & msg) { std::cout << msg.str() << std::endl; };
+	logger::g_level1<char> = [](std::basic_stringstream<char> & msg) { std::cout << msg.str() << std::endl; };
+}
+
+#endif
+
 using logger::hex;
 using logger::what;
 
@@ -8,10 +43,9 @@ int main()
 {
 	std::setlocale(LC_ALL, "");
 
-	logger::Initialize(3);
+	Initialize();
 
 	// TODO cassert で簡単なテストを追加する
-
 	INFO("INFO", hex(0x01234567));
 	WARN("WARN", hex(0x01234567));
 	FAIL("FAIL", hex(0x01234567));
@@ -20,8 +54,6 @@ int main()
 	WARN("WARN", what(1));
 #if  defined(WIN32)
 	FAIL("FAIL", what(E_FAIL));
-#else
-	FAIL("FAIL", what(-1));
 #endif
 
 	return 0;
